93bc12cc4388c3f1544e100d45dd50d6de8061f6
Mohd-Akbar1 <146831190+Mohd-Akbar1@users.noreply.github.com>
2025-01-05 16:50:02 +0530

    Updated frontend new url

--- Feedback/src/controller/user.ts
+++ Feedback/src/controller/user.ts
@@ -1,57 +1,4 @@
-// import { Request, Response } from "express";
-// import postgresdb from "../db/db";
-// import  {user}  from "../model/model";
-// import db from "../db/db";
-// import { eq, sql } from "drizzle-orm";
 
-
-// export const userController = async (req: Request, res: Response) => {
-//     try {
-//         const  name :string = req.body.name; 
-//         if(!name){
-//             return res.status(400).json({error:"name is required"});
-//         }
-//         console.log(name) 
-        
-       
-//         const nameId =await db.insert(user).values({ name: name,message:["test"] }).returning();
-//         const baseAPI="http://localhost:3000"+"/chat/"+nameId[0].id
-//         return res.status(200).json({ Link: baseAPI ,userId:nameId[0].id,name :user.name});
-
-//     } catch (error: any) {
-//         console.log(error);
-//         return res.status(500).json({ error: error.message }); 
-//     }
-// };
-
-// export const usermessage=async(req:Request,res:Response)=>{
-//     try{
-//         const {id}=req.params;
-//         const newMessage=req.body.message
-
-//         const updatedUser = await db
-//             .update(user)
-//             .set({
-//                 message: sql`array_append(${user.message}, ${newMessage})`
-//             })
-//             .where(eq(user.id, id)) 
-//             .returning(); ; 
-//         return res.status(200).json({message:"comment updated"});
-//     }catch(error:any){
-//         console.log(error);
-//         return res.status(500).json({error:error.message});
-//     }
-// }
-// export const allusermessage=async(req:Request,res:Response)=>{
-//     try{
-//         const userID=req.params.id
-//         const users=await db.select({Message:user.message}).from(user).where(eq(user.id,req.params.id));
-//         return res.status(200).json({message:users[0].Message});
-//     }catch(error:any){
-//         console.log(error);
-//         return res.status(500).json({error:error.message});
-//     }
-// }   
 
 
 import { Request, Response } from "express";
@@ -72,7 +19,7 @@ export const userController = async (req: Request, res: Response) => {
         const nameId = await db.insert(user).values({ name }).returning();
 
         // Generate base API link
-        const baseAPI = `https://anon-chat-omega.vercel.app/chat/${nameId[0].id}`;
+        const baseAPI = `https://anon-chat-five.vercel.app/chat/${nameId[0].id}`;
 
         // Sanitize the response to avoid circular references
         return res.status(200).json({
